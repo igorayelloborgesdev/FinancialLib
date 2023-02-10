c923928a8b176ada8ab92d4d6f8a7c9aa714e94b
Igor <igorayelloborgesdev@gmail.com>
2023-02-09 22:37:23 -0300

    Clean main script

--- scriptTest.js
+++ scriptTest.js
@@ -1,17 +1,1 @@
 const FinancialLib = require('./main');
-
-// var interest = 6;
-// var currentValue = 2000.00;
-// var period = 5;
-// console.log(FinancialLib.Capitalization.CapitalizationSimplePeriod(currentValue, interest, period));//Juros simples
-// console.log(FinancialLib.Capitalization.CapitalizationCompound(currentValue, interest, period));
-// console.log(FinancialLib.Capitalization.NominalYield(5000.00, 5190.00));
-// console.log(FinancialLib.Capitalization.CapitalizationSimplePeriod(6000.00, 4.0, 5));//Juros simples
-// console.log(FinancialLib.Capitalization.CapitalizationSimpleFutureValue(8000.00, 4.0, 6));//Juros simples valor futuro
-// console.log(FinancialLib.Capitalization.CapitalizationSimpleFutureValue(8000.00, 4.0, 6));
-// console.log(FinancialLib.Capitalization.RealInterestRateByCurrentValueFutureValueAndPeriod(15000.00, 17100.00, 105/30));
-// console.log(FinancialLib.Capitalization.NominalInterestRate(28.00, 7.0));
-// console.log(FinancialLib.Capitalization.RealIncome(6.59, 4.5));
-// console.log(FinancialLib.Capitalization.NominalInterestRate(0.60, 15.00));
-// console.log(FinancialLib.Capitalization.RealIncome(6.89, 5.0));
-// console.log(FinancialLib.Capitalization.NominalInterestRateWithCurrentValue(1000.0, 6.0, 35.0));
